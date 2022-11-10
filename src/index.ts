c5556b99481b30ec7092e8537325685571b7cb05
Roberto <cacaracteres86@gmail.com>
2022-11-10 17:57:50 +0100

    retoque de la base echa

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,25 @@ import http from 'http';
 import * as dotenv from 'dotenv';
 const numberA = 5;
 const numberB = 6;
+const sum = numberA + numberB;
+const rest = numberA - numberB;
+const multiply = numberA * numberB;
+const divison = numberA / numberB;
 
 dotenv.config();
 const port = 3100;
 const server = http.createServer((request, response) => {
     request.method;
 
-    request.url = 'google.com';
+    request.url;
 
-    response.write(`${numberA} + ${numberB}`);
-    response.write(`${numberA} - ${numberB}`);
-    response.write(`${numberA} * ${numberB}`);
-    response.write(`${numberA} / ${numberB}`);
+    response.write(`${numberA} + ${numberB} = ${sum}`);
+    response.write('        ');
+    response.write(`${numberA} - ${numberB} = ${rest}`);
+    response.write('        ');
+    response.write(`${numberA} * ${numberB} = ${multiply}`);
+    response.write('        ');
+    response.write(`${numberA} / ${numberB} = ${divison}`);
     response.end();
 });
 
